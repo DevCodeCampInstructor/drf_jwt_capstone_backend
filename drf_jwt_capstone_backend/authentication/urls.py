b7b3ab9a298e84d159152ff1624eda19df46062b
Cash Myers <cmyers880@gmail.com>
2021-12-07 08:11:03 -0600

    Added custom claims for JWT. Includes user id, username, and email. Added endpoints for user list, user by id, and user by username

--- drf_jwt_capstone_backend/authentication/urls.py
+++ drf_jwt_capstone_backend/authentication/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, UserDetail, UserList, CustomTokenObtainPairView, CustomTokenObtainPairRefreshView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', CustomTokenObtainPairRefreshView.as_view(),
+         name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('userid/', UserList.as_view(), name="userlist"),
+    path('user/<int:pk>', UserDetail.as_view(), name='getuser'),
+    path('username/<str:username>', UserDetail.as_view(), name='get_user_by_name'),
 ]
